47325857cb018d4aa8c1d2175928ebd491b2c166
Ashish choudhary <ashish7232816@gmail.com>
2024-07-09 17:47:40 +0530

    change navbar

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <div className=' navbar'>
         <img className='logo' src={assets.logo} alt="logo" />
-        <h5>Admin-Page</h5>
+        <h5>Admin</h5>
         <img className='profile' src={assets.profile_image} alt="admin img" />
       
     </div>
